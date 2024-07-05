db7939658ecf2dc7e574c48c2e95583428d05cd6
Avindu21894 <a.payagalagegr52017@gmail.com>
2024-07-05 12:43:04 +0530

    Company Signup Page

--- src/app/company-signup/page.tsx
+++ src/app/company-signup/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import NavScrollExample from '../components/navbar/navbar'
 import Footer from '../components/footer/footer'
+import CompSign from '../components/company-signup/comp_sign'
 
 export default function CompSignUp() {
   return (
     <>
     <NavScrollExample />
+    <CompSign />
     <Footer />
     </>
   )
