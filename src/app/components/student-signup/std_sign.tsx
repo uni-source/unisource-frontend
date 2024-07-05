6570c8adae99d6481639da0ca6b1ccb1a9e3b72f
Avindu21894 <a.payagalagegr52017@gmail.com>
2024-07-05 12:29:51 +0530

    Student Signup Page

--- src/app/components/student-signup/std_sign.tsx
+++ src/app/components/student-signup/std_sign.tsx
@@ -1,58 +1,63 @@
-// 'use client';
-// import * as React from 'react';
-// import './std_sign.css'
-// import CssBaseline from '@mui/material/CssBaseline';
-// import TextField from '@mui/material/TextField';
-// import Box from '@mui/material/Box';
-// import Typography from '@mui/material/Typography';
-// import Container from '@mui/material/Container';
-// import CustomIcon from '../custom_icon/customicon';
+'use client';
+import * as React from 'react';
+import './std_sign.css'
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import CustomIcon from '../custom_icon/customicon';
 
 
-// export default function StdSignUp() {
+export default function StdSign() {
 
-//   return (
-//       <Container component="main" maxWidth="xs">
-//         <CssBaseline />
-//         <Box className="form-border"
-//           sx={{
-//             marginTop: 8,
-//             display: 'flex',
-//             flexDirection: 'column',
-//             alignItems: 'center',
-//           }}
-//         >
-//             <CustomIcon />
-//           <Typography component="h1" variant="h5">
-//             Sign Up
-//           </Typography>
-//           <Box component="form" noValidate sx={{ mt: 1 }}>
-//             <TextField
-//               className='custom-text-field-color'
-//               margin="normal"
-//               required
-//               fullWidth
-//               id="email"
-//               label="Email Address"
-//               name="email"
-//               autoComplete="email"
-//               autoFocus
-//             />
-//             <TextField
-//               className='custom-text-field-color'
-//               margin="normal"
-//               required
-//               fullWidth
-//               name="password"
-//               label="Password"
-//               type="password"
-//               id="password"
-//               autoComplete="current-password"
-//             />
-//             <br />
-//             <button className="submit-btn">SIGN UP</button>
-//           </Box>
-//         </Box>
-//       </Container>
-//   );
-// }
+  return (
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box className="form-border"
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+            <CustomIcon />
+          <Typography component="h1" variant="h5">
+            Sign Up
+          </Typography>
+          <Box component="form" noValidate sx={{ mt: 1 }}>
+            <TextField
+              className='custom-text-field-color'
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              className='custom-text-field-color'
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            /><br />
+            <button className="submit-btn">SIGN UP</button>
+          </Box>
+        </Box>
+      </Container>
+  );
+}
