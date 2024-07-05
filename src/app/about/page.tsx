fd1988c2c38970e3baa5b513303b219f77379086
Geenuka2020 <geenuka2020@gmail.com>
2024-07-05 20:47:20 +0530

    About-Mission-Created

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,12 +4,14 @@ import Topic from '../components/topics/topics';
 import NavScrollExample from '../components/navbar/navbar';
 import Footer from '../components/footer/footer';
 import AboutHeader from '../components/AboutHeader/AboutHeader';
+import Mission from '../components/Mission/Mission';
 
 const About = () => {
   return (
     <>
     <NavScrollExample />
     <AboutHeader/>
+    <Mission/>
     <Footer />
     </>
   )
