f04cc95bffda1bc5e46cd8c9ef662518940cd850
Avindu21894 <a.payagalagegr52017@gmail.com>
2024-07-06 13:38:37 +0530

    Form Validation - Student and Company Log In

--- src/app/components/company-login/comp_log.tsx
+++ src/app/components/company-login/comp_log.tsx
@@ -1,63 +1,88 @@
 'use client';
 import * as React from 'react';
-import './comp_log.css'
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import './comp_log.css';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import CustomIcon from '../custom_icon/customicon';
 
+// Yup Validation
+const validationSchema = Yup.object({
+  email: Yup.string()
+    .email('Invalid email address')
+    .required('Email is required'),
+  password: Yup.string()
+    .min(8, 'Password must be at least 8 characters')
+    .required('Password is required'),
+});
 
 export default function CompLogIn() {
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
 
   return (
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box className="form-border"
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-            <CustomIcon />
-          <Typography component="h1" variant="h5">
-            Login
-          </Typography>
-          <Box component="form" noValidate sx={{ mt: 1 }}>
-            <TextField
-              className='custom-text-field-color'
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              className='custom-text-field-color'
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            /><br />
-            <button className="submit-btn">LOG IN</button>
-          </Box>
-        </Box>
-      </Container>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        className="form-border"
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <CustomIcon />
+        <Typography component="h1" variant="h5">
+          Login
+        </Typography>
+        <form onSubmit={formik.handleSubmit} noValidate >
+          <TextField
+            className='custom-text-field-color'
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            error={formik.touched.email && Boolean(formik.errors.email)}
+            helperText={formik.touched.email && formik.errors.email}
+          />
+          <TextField
+            className='custom-text-field-color'
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            value={formik.values.password}
+            onChange={formik.handleChange}
+            error={formik.touched.password && Boolean(formik.errors.password)}
+            helperText={formik.touched.password && formik.errors.password}
+          />
+          <br />
+          <button className="submit-btn" type="submit">LOG IN</button>
+        </form>
+      </Box>
+    </Container>
   );
 }
