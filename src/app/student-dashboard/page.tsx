1e396adf6db73d401097c1346bbb48028a4cf33d
Avindu21894 <a.payagalagegr52017@gmail.com>
2024-07-09 16:18:09 +0530

    student-dashboard folder structure and side navigation panel

---
+++ src/app/student-dashboard/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import MiniDrawer from '../components/dashboard/student-dashboard/side-nav/sidenav'
+
+export default function page() {
+  return (
+    <MiniDrawer />
+  )
+}
