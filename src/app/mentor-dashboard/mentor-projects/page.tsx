a2ca20c308afab43bc50259f0ea9a8cdc9c8287e
Avindu21894 <a.payagalagegr52017@gmail.com>
2024-07-31 13:47:15 +0530

    mentor all project update

--- src/app/mentor-dashboard/mentor-projects/page.tsx
+++ src/app/mentor-dashboard/mentor-projects/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import MiniDrawer from "@/app/components/dashboard/organization-dashboard/side-nav/sidenav";
+import MiniDrawer from "@/app/components/dashboard/mentor-dashboard/side-nav/sidenav";
 import Box from "@mui/material/Box";
 import Loading from "@/app/components/loading/loading";
 import { useGetOrganizationQuery } from "../../../../redux/features/organization/organizationApi";
@@ -36,7 +36,7 @@ const Page: React.FC = () => {
     <Box sx={{ display: "flex" }}>
       <MiniDrawer
         childTitle="All Projects"
-        organization={organization}
+        mentor={organization}
       />
       <Box component="main" sx={{ flexGrow: 1, p: 3, marginTop: 4,'@media (max-width: 600px)': {
             width: 320,
