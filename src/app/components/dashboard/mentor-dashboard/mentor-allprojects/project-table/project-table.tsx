bcc1a9618c10b10001f0ed3e12fe0b448bb092a4
Avindu21894 <a.payagalagegr52017@gmail.com>
2024-07-31 11:49:37 +0530

    mentor-all projects page created

---
+++ src/app/components/dashboard/mentor-dashboard/mentor-allprojects/project-table/project-table.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import './project-table.css';
+import Link from 'next/link';
+
+const ProjectGrid: React.FC = () => {
+  return (
+    <div className="container project-form-container">
+      <form>
+        <div className="form-row">
+          <div className="form-group col-md-4">
+            <label htmlFor="projectName">Project Name</label>
+            <input type="text" className="form-control" id="projectName" />
+          </div>
+          <div className="form-group col-md-4">
+            <label htmlFor="title">Title</label>
+            <input type="text" className="form-control" id="title" />
+          </div>
+          <div className="form-group col-md-4">
+            <label htmlFor="dueDate">Category</label>
+            <input type="text" className="form-control" id="dueDate" />
+          </div>
+        </div>
+        <div className="form-group">
+          <label htmlFor="description">Description</label>
+          <textarea className="form-control" id="description" rows={4}></textarea>
+        </div>
+        <div className="form-group">
+          <label htmlFor="technology">Technology</label>
+          <textarea className="form-control" id="description" rows={4}></textarea>
+        </div>
+        <div className="form-row">
+          <div className="form-group col-md-6">
+            <label htmlFor="dueDate">Due Date</label>
+            <input type="text" className="form-control" id="dueDate" />
+          </div>
+          <div className="form-group col-md-6">
+            <label htmlFor="Resources">Resources</label>
+            <input type="email" className="form-control" id="resorces" />
+          </div>
+        </div>
+        <Link href='/organization-dashboard/organization-proposals'>
+        <button type="button" className="btn btn-back">Back</button>
+        </Link>
+      </form>
+    </div>
+  );
+};
+
+
+export default ProjectGrid;
