0bd82395d45c87ac6983d3d604c996630a7a8a26
Sandunika Nawullage <sandunikanawullage201@gmail.com>
2024-08-12 20:43:32 -0700

    navbar updation

--- src/app/mentor-dashboard/mentor-projects/page.tsx
+++ src/app/mentor-dashboard/mentor-projects/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import MiniDrawer from "@/app/components/dashboard/organization-dashboard/side-nav/sidenav";
+import MiniDrawer from "@/app/components/dashboard/mentor-dashboard/side-nav/sidenav";
 import Box from "@mui/material/Box";
 import Loading from "@/app/components/loading/loading";
 import { useGetOrganizationQuery } from "../../../../redux/features/organization/organizationApi";
@@ -19,7 +19,7 @@ const Page: React.FC = () => {
   });
 
   const {
-    data: organization,
+    data: mentor,
     isLoading,
     refetch,
   } = useGetOrganizationQuery(userId, { refetchOnMountOrArgChange: true });
@@ -36,7 +36,7 @@ const Page: React.FC = () => {
     <Box sx={{ display: "flex" }}>
       <MiniDrawer
         childTitle="All Projects"
-        organization={organization}
+        mentor={mentor}
       />
       <Box component="main" sx={{ flexGrow: 1, p: 3, marginTop: 4,'@media (max-width: 600px)': {
             width: 320,
